96acebec2e77ed2e46fd3523362a1dd5aa6638ff
Andrew De Ponte <cyphactor@gmail.com>
2017-11-15 22:45:03 -0800

    Initial Commit

---
+++ Printable/Printable.h
@@ -0,0 +1,19 @@
+//
+//  Printable.h
+//  Printable
+//
+//  Created by Andrew De Ponte on 11/15/17.
+//  Copyright © 2017 UpTech Works, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Printable.
+FOUNDATION_EXPORT double PrintableVersionNumber;
+
+//! Project version string for Printable.
+FOUNDATION_EXPORT const unsigned char PrintableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Printable/PublicHeader.h>
+
+
