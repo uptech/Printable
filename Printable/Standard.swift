c35676a0e181fd1bffd98eed0a2ea0aa069b707e
Andrew De Ponte <cyphactor@gmail.com>
2017-11-15 22:55:34 -0800

    Setup Playground for an example
    I did this so that I could test it easily.

---
+++ Printable/Standard.swift
@@ -0,0 +1,13 @@
+//
+//  Standard.swift
+//  Printable
+//
+//  Created by Andrew De Ponte on 11/15/17.
+//  Copyright © 2017 UpTech Works, LLC. All rights reserved.
+//
+
+import Foundation
+
+public func helloWorld() {
+    print("Printable: Hello World!")
+}
